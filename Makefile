5bba1ebbca4c5984bf2ed7669240450aad72c810
mtgto <hogerappa@gmail.com>
2019-02-23 20:48:22 +0900

    Add Dockerfile for data

--- Makefile
+++ Makefile
@@ -20,6 +20,7 @@ $(WEB): cmd/web/main.go $(wildcard internal/app/core/*.go) $(wildcard internal/a
 $(BENCH): cmd/bench/main.go $(wildcard internal/app/core/*.go)
 	$(GO) build -ldflags "$(LDFLAGS)" -o $@ $<
 
+.PHONY: test-web
 test-web:
 	$(GO) test github.com/mtgto/pediaroute-go/internal/app/web
 
--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"os"
 
 	"github.com/mtgto/pediaroute-go/internal/app/gen"
@@ -11,18 +12,25 @@ import (
 func main() {
 	var (
 		language     = flag.String("lang", "", "Language ID (ja or en)")
-		pageFile     = flag.String("ip", "", "File path of pages.sql.gz")
-		pageLinkFile = flag.String("il", "", "File path of pages.sql.gz")
+		pageFile     = flag.String("ip", "", "File path of page.sql.gz")
+		pageLinkFile = flag.String("il", "", "File path of pagelinks.sql.gz")
 		outDir       = flag.String("o", "", "Output directory")
 	)
 	flag.Parse()
-	if fileMode, err := os.Stat(*pageFile); err == nil && fileMode.Mode().IsRegular() {
-		if fileMode, err := os.Stat(*pageLinkFile); err == nil && fileMode.Mode().IsRegular() {
-			if fileMode, err := os.Stat(*outDir); err == nil && fileMode.IsDir() {
-				gen.Run(*language, *pageFile, *pageLinkFile, *outDir)
-				os.Exit(0)
-			}
-		}
+	if fileMode, err := os.Stat(*pageFile); err != nil || !fileMode.Mode().IsRegular() {
+		log.Println("No page sql file.")
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-	flag.PrintDefaults()
+	if fileMode, err := os.Stat(*pageLinkFile); err != nil || !fileMode.Mode().IsRegular() {
+		log.Println("No pagelinks sql file.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if fileMode, err := os.Stat(*outDir); err != nil || !fileMode.IsDir() {
+		log.Println("No output directory.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	gen.Run(*language, *pageFile, *pageLinkFile, *outDir)
 }
