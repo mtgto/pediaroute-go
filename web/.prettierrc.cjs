9ba48df1640d8f7c93e027c75b7d9b5cce4c23e9
mtgto <hogerappa@gmail.com>
2022-09-04 13:11:18 +0900

    Fix wrong prettierrc

--- web/.prettierrc.cjs
+++ web/.prettierrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
-  extends: ['prettier', 'prettier/@typescript-eslint'],
   printWidth: 150,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: true,
+  trailingComma: 'all',
 };
