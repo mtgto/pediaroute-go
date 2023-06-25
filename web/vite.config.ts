ce87abf75b17729aa95324ade32bf598fe85b15e
mtgto <hogerappa@gmail.com>
2023-06-25 23:58:43 +0900

    Vite 4にバージョンアップ

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,13 @@
-
 import { defineConfig, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 // https://vitejs.dev/config/
 export const config: UserConfigExport = {
-  plugins: [vue(), vueI18n()],
+  plugins: [vue()],
   server: {
     proxy: {
       '/api': 'https://pediaroute.com',
-    }
+    },
   },
 };
 export default defineConfig(config);
